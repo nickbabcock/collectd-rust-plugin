b793b1a6cb21e005fed6fc5d27608a3195bc042b
Nick Babcock <nbabcock19@hotmail.com>
2017-08-27 17:39:05 -0400

    Remove unnecessary comments

--- build.rs
+++ build.rs
@@ -4,17 +4,7 @@ use std::env;
 use std::path::PathBuf;
 
 fn main() {
-    // Tell cargo to tell rustc to link the system bzip2
-    // shared library.
-//    println!("cargo:rustc-link-lib=collectd");
-//    println!("cargo:rustc-link-search=native=/home/nick/projects/collectd");
-
-    // The bindgen::Builder is the main entry point
-    // to bindgen, and lets you build up options for
-    // the resulting bindings.
     let bindings = bindgen::Builder::default()
-        // The input header we would like to generate
-        // bindings for.
         .header("wrapper.h")
         .clang_arg("-DHAVE_CONFIG_H")
         .unstable_rust(true)
@@ -25,9 +15,7 @@ fn main() {
         .hide_type("FP_NORMAL")
         .hide_type("max_align_t")
         .hide_type("module_register")
-        // Finish the builder and generate the bindings.
         .generate()
-        // Unwrap the Result and panic on failure.
         .expect("Unable to generate bindings");
 
     // Write the bindings to the $OUT_DIR/bindings.rs file.
