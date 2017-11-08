c628599817efcf4b805b6031286717b46fce9b96
Nick Babcock <nbabcock19@hotmail.com>
2017-11-08 18:24:12 -0500

    Add travis test

---
+++ ci/test.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+set -euo pipefail
+cargo test --features $VERSION
+
+cp target/debug/libmyplugin.so /usr/lib/collectd/myplugin.so
+
+cat <<EOF >/etc/collectd/collectd.conf
+Hostname "localhost"
+LoadPlugin myplugin
+LoadPlugin csv
+<Plugin csv>
+  DataDir "/var/lib/collectd/csv"
+  StoreRates false
+</Plugin>
+<Plugin myplugin>
+  Short "2"
+  Long "5.5"
+</Plugin>
+EOF
+
+service collectd start
+sleep 5
+
+grep '2.000000,10.000000,5.500000' /var/lib/collectd/csv/localhost/myplugin/load*
+
