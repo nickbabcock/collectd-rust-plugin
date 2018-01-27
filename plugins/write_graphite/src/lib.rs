cb2d9b9e1f62bb03d535605405d67a53aae74a59
Nick Babcock <nbabcock19@hotmail.com>
2018-01-27 14:33:54 -0500

    Add write_graphite example plugin

---
+++ plugins/write_graphite/src/lib.rs
@@ -0,0 +1,136 @@
+#[macro_use]
+extern crate collectd_plugin;
+extern crate failure;
+extern crate serde;
+#[macro_use]
+extern crate serde_derive;
+
+use collectd_plugin::{collectd_log, ConfigItem, LogLevel, Plugin, PluginCapabilities,
+                      PluginManager, PluginRegistration, RecvValueList, Value};
+use failure::Error;
+use std::sync::Mutex;
+use std::net::TcpStream;
+use std::io::Write;
+use std::borrow::Cow;
+use std::ops::Deref;
+
+#[derive(Deserialize, Debug, PartialEq, Default)]
+#[serde(rename_all = "PascalCase")]
+#[serde(deny_unknown_fields)]
+struct GraphiteConfig {
+    name: String,
+    address: String,
+    prefix: Option<String>,
+}
+
+struct GraphitePlugin<W: Write + Send> {
+    writer: Mutex<W>,
+    prefix: Option<String>,
+}
+
+struct GraphiteManager;
+
+impl PluginManager for GraphiteManager {
+    fn name() -> &'static str {
+        "write_graphite_rust"
+    }
+
+    fn plugins(config: Option<&[ConfigItem]>) -> Result<PluginRegistration, Error> {
+        // Deserialize the collectd configuration into our configuration struct
+        let config: Vec<GraphiteConfig> =
+            collectd_plugin::de::from_collectd(config.unwrap_or_else(Default::default))?;
+
+        let config: Result<Vec<(String, Box<Plugin>)>, Error> = config
+            .into_iter()
+            .map(|x| {
+                let plugin = GraphitePlugin {
+                    writer: Mutex::new(TcpStream::connect(x.address)?),
+                    prefix: x.prefix,
+                };
+                let bx: Box<Plugin> = Box::new(plugin);
+                Ok((x.name.clone(), bx))
+            })
+            .collect();
+
+        Ok(PluginRegistration::Multiple(config?))
+    }
+}
+
+fn graphitize(s: &str) -> Cow<str> {
+    let needs_modifying = s.chars()
+        .any(|x| x == '.' || x.is_whitespace() || x.is_control());
+    if !needs_modifying {
+        Cow::Borrowed(s)
+    } else {
+        let new_s: String = s.chars()
+            .map(|x| {
+                if x == '.' || x.is_whitespace() || x.is_control() {
+                    '-'
+                } else {
+                    x
+                }
+            })
+            .collect();
+        Cow::Owned(new_s)
+    }
+}
+
+impl<W: Write + Send> GraphitePlugin<W> {
+    fn write_value(&mut self, mut line: String, val: Value, dt: &str) {
+        line.push(' ');
+        line.push_str(val.to_string().as_str());
+        line.push(' ');
+        line.push_str(dt);
+        line.push('\n');
+        let mut w = self.writer.lock().unwrap();
+        if let Err(ref e) = w.write(line.as_bytes()) {
+            collectd_log(LogLevel::Error, e.to_string().as_str());
+        }
+    }
+}
+
+impl<W: Write + Send> Plugin for GraphitePlugin<W> {
+    fn capabilities(&self) -> PluginCapabilities {
+        PluginCapabilities::WRITE
+    }
+
+    fn write_values<'a>(&mut self, list: RecvValueList<'a>) -> Result<(), Error> {
+        let mut line = String::new();
+        if let Some(ref prefix) = self.prefix {
+            line.push_str(prefix.as_str());
+            line.push('.');
+        }
+        line.push_str(graphitize(list.host).deref());
+        line.push('.');
+        line.push_str(graphitize(list.plugin).deref());
+
+        if let Some(instance) = list.plugin_instance {
+            line.push('-');
+            line.push_str(graphitize(instance).deref());
+        }
+
+        line.push('.');
+        line.push_str(graphitize(list.type_).deref());
+
+        if let Some(type_instance) = list.type_instance {
+            line.push('-');
+            line.push_str(graphitize(type_instance).deref());
+        }
+
+        let dt = list.time.timestamp().to_string();
+        if list.values.len() == 1 {
+            self.write_value(line, list.values[0].value, dt.as_str());
+        } else {
+            for v in list.values {
+                let mut nv = line.clone();
+                nv.push('.');
+                nv.push_str(graphitize(v.name).deref());
+                self.write_value(nv, v.value, dt.as_str());
+            }
+        }
+
+        Ok(())
+    }
+}
+
+collectd_plugin!(GraphiteManager);
