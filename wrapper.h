124c6280f4950b03ab4694aef47a057ed341c218
Nick Babcock <nbabcock19@hotmail.com>
2018-01-29 18:14:11 -0500

    Simplify wrapper.h for collectd-5.8

--- wrapper.h
+++ wrapper.h
@@ -1,12 +1,6 @@
-#ifdef COLLECTD_55
-    #include <collectd/liboconfig/oconfig.h>
-    #include <collectd/core/daemon/plugin.h>
-#endif
-
 #ifdef COLLECTD_54
     #include <collectd/core/plugin.h>
-#endif
-
-#ifdef COLLECTD_57
+#else
+    #include <collectd/liboconfig/oconfig.h>
     #include <collectd/core/daemon/plugin.h>
 #endif
