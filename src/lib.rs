4d23ff340e3b1e71a5cf8edf1e357a152b077581
Nick Babcock <nbabcock19@hotmail.com>
2018-03-09 08:10:28 -0500

    Bump to 0.4.3

--- src/lib.rs
+++ src/lib.rs
@@ -17,7 +17,7 @@
 //!
 //! ```toml
 //! [dependencies]
-//! collectd-plugin = "0.4.2"
+//! collectd-plugin = "0.4.3"
 //! ```
 //!
 //! If you want [Serde](https://github.com/serde-rs/serde) support (recommended), include:
@@ -25,7 +25,7 @@
 //!
 //! ```toml
 //! [dependencies.collectd-plugin]
-//! version = "0.4.2"
+//! version = "0.4.3"
 //! features = ["serde"]
 //! ```
 //!
