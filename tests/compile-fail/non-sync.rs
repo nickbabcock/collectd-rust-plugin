eb8bc493434021ffe0db5f682e6bd2bd2652b22f
Nick Babcock <nbabcock19@hotmail.com>
2018-10-23 07:01:48 -0500

    Add compiletest tests

---
+++ tests/compile-fail/non-sync.rs
@@ -0,0 +1,19 @@
+extern crate collectd_plugin;
+extern crate failure;
+use std::cell::RefCell;
+use std::collections::HashSet;
+use collectd_plugin::Plugin;
+use failure::Error;
+
+#[derive(Debug, Default)]
+pub struct MyPlugin {
+    names: RefCell<HashSet<String>>,
+}
+
+impl Plugin for MyPlugin {
+//~^ error: std::cell::RefCell<std::collections::HashSet<std::string::String>>` cannot be shared between threads safely [E0277]
+    fn read_values(&self) -> Result<(), Error> {
+        let mut n = self.names.borrow_mut();
+        n.insert(String::from("A"));
+    }
+}
