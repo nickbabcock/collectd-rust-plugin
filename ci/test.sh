5a7c82cdb1860463da036e04eff239e1633a99cc
Nick Babcock <nbabcock19@hotmail.com>
2018-11-25 12:00:07 -0600

    Ensure that configuration is called for loggers

--- ci/test.sh
+++ ci/test.sh
@@ -17,6 +17,9 @@ LoadPlugin write_log
 LoadPlugin csv
 LoadPlugin logfile
 
+<Plugin write_log>
+    LogTimings "INFO"
+</Plugin>
 <Plugin logfile>
     LogLevel info
     File "/var/lib/collectd/log"
@@ -36,9 +39,14 @@ sleep 15
 service collectd status
 service collectd stop
 
-grep 'epoch,shortterm,midterm,longterm' /var/lib/collectd/csv/localhost/loadrust/load*
-grep 'collectd logging configuration: None' /var/lib/collectd/log
-grep 'testwriteplugin: write_log: rust logging configuration: None' /var/lib/collectd/log
-grep 'testwriteplugin: write_log: flushing: timeout: no timeout, identifier: no identifier' /var/lib/collectd/log
-grep 'testwriteplugin: write_log: yes drop is called' /var/lib/collectd/log
+grep_test() {
+    echo "looking for $1 in $2"
+    grep $1 $2
+}
+
+grep_test 'epoch,shortterm,midterm,longterm' /var/lib/collectd/csv/localhost/loadrust/load*
+grep_test 'collectd logging configuration: Some' /var/lib/collectd/log
+grep_test 'testwriteplugin: write_log: rust logging configuration: Some' /var/lib/collectd/log
+grep_test 'testwriteplugin: write_log: flushing: timeout: no timeout, identifier: no identifier' /var/lib/collectd/log
+grep_test 'testwriteplugin: write_log: yes drop is called' /var/lib/collectd/log
 exit $?
