6b9cc0d11cc9d2c90659eb893c52861297792661
Nick Babcock <nbabcock19@hotmail.com>
2018-12-01 17:41:46 -0600

    Fix null guard check for message logging

--- src/plugins.rs
+++ src/plugins.rs
@@ -202,7 +202,7 @@ macro_rules! collectd_plugin {
 
             // Guard against potential null messages even if they are not supposed to happen.
             if message.is_null() {
-                return 0;
+                return;
             }
 
             let msg = unsafe { CStr::from_ptr(message).to_string_lossy() };
