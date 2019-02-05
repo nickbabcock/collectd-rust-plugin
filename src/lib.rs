f670ff6af48b21e7c031313c65442d8bb13df195
Nick Babcock <nbabcock19@hotmail.com>
2019-02-04 18:25:01 -0600

    Release 0.9.1

--- src/lib.rs
+++ src/lib.rs
@@ -18,7 +18,7 @@
 //!
 //! ```toml
 //! [dependencies]
-//! collectd-plugin = "0.9.0"
+//! collectd-plugin = "0.9.1"
 //! ```
 //!
 //! [Serde](https://github.com/serde-rs/serde) support is enabled by default for configuration parsing.
