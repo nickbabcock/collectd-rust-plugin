c6c6b73e5abfce79a95b404c1dae22905c0687fd
Nick Babcock <nbabcock19@hotmail.com>
2024-02-16 06:21:04 -0600

    ...

--- tests/compiles.rs
+++ tests/compiles.rs
@@ -1,8 +1,5 @@
-#[rustversion::attr(not(stable), ignore)]
 #[test]
 fn ui() {
-    if std::env::var("CI").is_ok() {
-        let t = trybuild::TestCases::new();
-        t.compile_fail("tests/compile-fail/*.rs");
-    }
+    let t = trybuild::TestCases::new();
+    t.compile_fail("tests/compile-fail/*.rs");
 }
